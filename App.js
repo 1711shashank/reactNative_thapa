d870f35fb2635e0b15a0fa0d6a6d950d70bc6028
Kumar Shashank <1711shashank@gmail.com>
2022-12-31 15:34:41 +0530

    1st Page

--- App.js
+++ App.js
@@ -1,22 +1,62 @@
-//1 import
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Home from './src/screens/Home';
 
-//2 create component that return JSX / simple function
-const App = () => {
+export default function App() {
+  const description = 'A paragraph is a self-contained unit of discourse in writing dealing with a particular point or idea.'
   return (
-    <View style={StyleSheet.container}>
-      <Text style={styles.textStyle}> Hello Shashank </Text>
+    <View style={styles.mainContainer}>
+      <View style={styles.homeTop}>
+        <Image
+          style={styles.headerImage} 
+          resizeMode="contain"
+          source={require("./assets/Photo.jpg")}
+        />
+        <Text style={styles.mainHeader}>  Welcome  </Text>
+        <Text style={[styles.mainHeader,{fontSize:33, color:'4c5dab', marginTop:0}]}>
+          Kumar Shashank 
+        </Text>
+        <Text style={styles.paraStyle}> {description} </Text>
+      </View>
     </View>
   );
 }
 
-//3 create styleSheet
 const styles = StyleSheet.create({
-    textStyle: {
-      color:'green',
-    },
-});
+  mainContainer: {
+    height:"100%",
+    display:"flex",
+    justifyContent: "space-between",
+    paddingHorizontal:"20",
+    backgroundColor:"#fff",
+    textAlign:"center",
+  },
+  homeTop:{
+    display:"flex",
+    justifyContent:"center",
+    alignItems:"center",
+    paddingHorizontal:10,
+  },
+  headerImage:{
+    height: undefined,
+    width:"100%",
+    aspectRatio: 1,
+    display:"flex",
+    alignItems:"stretch",
+    marginTop:50,
+    borderRadius:20,
+  },
+  mainHeader:{
+    fontSize:30,
+    color:"#344055",
+    textTransform:"uppercase",
+  },
+  paraStyle:{
+    textAlign:"left",
+    fontSize:19,
+    color:"#7d7d7d",
+    marginTop:30,
+    paddingBottom:50,
+    lineHeight:26,
+  }
 
-//4 export the file
-export default App;
+});
